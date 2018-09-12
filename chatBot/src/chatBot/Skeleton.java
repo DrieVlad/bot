1a8188891be1809a5bc8c69436099b5173762e85
ekmust2499 <katiammm0204@mail.ru>
2018-09-12 17:47:44 +0500

    Merge branch 'master' of github.com:DrieVlad/bot
    # Conflicts:
    #	chatBot/src/chatBot/.gitignore

--- chatBot/src/chatBot/Skeleton.java
+++ chatBot/src/chatBot/Skeleton.java
@@ -1,34 +1,38 @@
 package chatBot;
+
 import java.util.Scanner;
 
 public class Skeleton {
-    private static String s_aboutMe = "Я бот, призванный играть с тобой";
+	private static String s_aboutMe = "Я бот, призванный играть с тобой";
+
 	public static void main(String[] args) {
 		System.out.println("Приветствуя тебя!");
 		System.out.println(s_aboutMe);
-		while (true) 
-		{
+		while (true) {
+
+			System.out.println("Хочешь ли ты поиграть со мной?");
+			Scanner input = new Scanner(System.in);
+			String s_useResponse = input.nextLine();
+			if (s_useResponse.equals("yes")) 
+			{
+				System.out.println("Сейчас поиграем");
+				System.out.println("Во что будем играть?");
+				System.out.println("У меня есть интересная игра в \"Города\"");
+				City = new Towns();
+
+			} 
+			else if (s_useResponse.equals("no")) 
+			{
+				System.out.println("Извините, я умею только играть");
+
+			} 
+			else
+			{
+				System.out.println("Извините, я вас не понял");
+			}
 			
-		    System.out.println("Хочешь ли ты поиграть со мной?");
-		    Scanner input = new Scanner(System.in);
-		    String s_useResponse = input.nextLine();
-		    if (s_useResponse == "yes")
-		    {
-			    System.out.println("Сейчас поиграем");
-			    System.out.println("Во что будем играть?");
-			    
-		    }
-		    else if (s_useResponse == "no")
-		    {
-			    System.out.println("Извините, я умею только играть");
-		
-	     	}
-		    else 
-		    {
-		    	System.out.println("Извините, я вас не понял");
-		    }
+
 		}
-		
 
 	}
 
