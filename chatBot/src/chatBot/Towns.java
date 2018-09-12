66330269905303774e76dfc01ac3133a110106ac
ekmust2499 <katiammm0204@mail.ru>
2018-09-12 12:49:21 +0500

    Города

---
+++ chatBot/src/chatBot/Towns.java
@@ -0,0 +1,78 @@
+package chatBot;
+
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Towns {
+	private static ArrayList<String> usedCities = new ArrayList<String>();
+	
+	public static void main(String[] args)
+	{
+		String lastLetter = "A";
+		while(true)
+		{
+			try {
+	            File file = new File("C:\\Users\\1\\Chat-Bot\\bot\\chatBot\\" + lastLetter + ".txt");
+	            FileReader fr = new FileReader(file);
+	            BufferedReader reader = new BufferedReader(fr);
+	            String line = reader.readLine();
+	            String firstLetter = "";
+	            
+	            while (line != null) {
+	            	
+                    line = reader.readLine();
+                    System.out.println("Hi");
+	            	if (usedCities.indexOf(line) == -1)
+	            	{
+	            		System.out.println(line);
+                        usedCities.add(line);
+                        lastLetter = line.substring(line.length() - 1, line.length()).toLowerCase();
+	            		break;
+	            	}            		
+	            	else 
+	            		continue;	            	
+	            }
+	            System.out.println("Ваш ход!");
+	            Scanner input = new Scanner(System.in);	           
+                while(true) {
+                	String userTown = input.nextLine();   	            
+    	            firstLetter = userTown.substring(0, 1).toLowerCase();
+                	 if (!lastLetter.equals(firstLetter))
+                     {
+                     	System.out.println("Ты играешь не по правилам. Попробуй еще раз!");
+                     	continue;
+                     }
+     	            if (usedCities.indexOf(userTown) == -1)
+                 	{  	            	
+                        usedCities.add(userTown);
+                        lastLetter = userTown.substring(userTown.length() - 1, userTown.length()).toUpperCase();
+                 		break;
+                 	}            		
+                 	else 
+                 	{
+                 		System.out.println("Ты играешь не по правилам. Попробуй еще раз!");                     
+                 		continue;
+                 	}
+    	            
+                }
+                System.out.println("Мой ход!");
+                  
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+		}
+		
+
+	}
+
+
+}
