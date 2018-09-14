39a06123ffac745829021ebd5979f3ef0f11f2c7
ekmust2499 <katiammm0204@mail.ru>
2018-09-15 00:23:48 +0500

    Update

--- chatBot/src/chatBot/Skeleton.java
+++ chatBot/src/chatBot/Skeleton.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Skeleton 
 {
-    private static String s_aboutMe = 
-    		"Я бот Евлампий, призванный играть с тобой\n"
-			+ "Пиши мне help  любое время и я напомню как со мной общаться\n"
-			+ "Я буду предлагать тебе играть в разные игры\n"
-			+ "твоя задача выбрать игру и наслаждаться";
+    private static String s_aboutMe = "Я бот Евлампий, призванный играть с тобой:))\n"
+			+ "Пиши мне \"help\" в любое время и я напомню как со мной общаться.\n"
+			+ "Я буду предлагать тебе на выбор разные игры,\n"
+			+ "а твоя задача выбрать игру и наслаждаться:))\n"
+			+ "Хочешь ли ты поиграть со мной?";
     
 	public static void main(String[] args) 
 	{
-		System.out.println("Приветствуя тебя!");
+		System.out.println("Приветствую тебя, мой дорогой друг!");
 		help("help");
         start();
 	}
@@ -20,47 +20,36 @@ public class Skeleton
 	public static void start()
 	{
 		while (true) 
-		{
-			System.out.println("Хочешь ли ты поиграть со мной?");
-			Scanner inputUser = new Scanner(System.in);
-			String s_useResponse = inputUser.nextLine();
+		{			
+			Scanner input = new Scanner(System.in);
+			String s_useResponse = input.nextLine();
 			help(s_useResponse);
-			if (s_useResponse.equals("yes") 
-					|| s_useResponse.equals("да") 
-					|| s_useResponse.equals("Да")) 
+			if (s_useResponse.equals("yes") || s_useResponse.equals("Да") || s_useResponse.equals("да") || s_useResponse.equals("конечно"))  
 			{
-				System.out.println("Сейчас поиграем");
 				System.out.println("Во что будем играть?");
 				System.out.println("У меня есть интересная игра в \"Города\"");
-			    
-			    Towns.cities();
-
+			    Towns.gameOfCities();
 			} 
-			else if (s_useResponse.equals("no")) 
+			else if (s_useResponse.equals("no") || s_useResponse.equals("Нет") || s_useResponse.equals("нет")) 
 			{
-				System.out.println("Извините, я умею только играть");
-
+				System.out.println("Извините, я умею только играть:((");
 			} 
 			else
 			{
-				System.out.println("Извините, я вас не понял");
-			}
-			
+				System.out.println("Извините, я вас не понял:((");
+			}		
+			input.close();
 		}
 	}
 	
 	public static boolean help(String s_Chek) 
 	{
-		if (s_Chek.equals("") 
-				|| s_Chek.equals("help") 
-				|| s_Chek.equals("помоги")
-				|| s_Chek.equals("помощь") 
-				|| s_Chek.equals("расскажи о себе")) 
+		if (s_Chek.equals("") || s_Chek.equals("help") || s_Chek.equals("помоги")
+				|| s_Chek.equals("помощь") || s_Chek.equals("расскажи о себе")) 
 		{
 			System.out.println(s_aboutMe);
 			return true;
 		}
 		return false;
 	}
-
 }
--- chatBot/src/chatBot/Towns.java
+++ chatBot/src/chatBot/Towns.java
@@ -9,15 +9,110 @@ import java.nio.file.Files;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.stream.*;
-import java.io.File;
 import java.nio.charset.Charset;
 import java.lang.Character;
 
 public class Towns {
 	private static ArrayList<String> usedCities = new ArrayList<String>();
+	private static String firstLetter = "";
+	private static String lastLetter = "";
+	
+		
+	public static void gameOfCities()
+	{
+		String alphabet = "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЭЮЯ";
+		Random ran = new Random();
+		int linesCount = 0;
+		int count = ran.nextInt(28);
+		lastLetter = alphabet.substring(count, count+1);
+		String line = "";
+		while(true)
+		{
+			try 
+			{            
+				Path pathToCities = Paths.get(System.getProperty("user.dir") + "\\chatBot\\Города\\" + lastLetter + ".txt");
+				Stream<String> lines = Files.lines(pathToCities, Charset.defaultCharset());
+            	linesCount = (int)lines.count();		
+	            //String firstLetter = "";
+	            Boolean flag = false;
+	            while (flag != true ) 
+	            {	            	                              			
+	            	count = ran.nextInt(linesCount);
+					line = Files.lines(pathToCities, Charset.defaultCharset()).skip(count).findFirst().get();
+					if (checkTheWordInTheDictionary(line))
+	            	{            		
+                        System.out.println(line);
+                        lastLetter = check(lastLetter, line);
+        				flag = true;
+	            		break;
+	            	}  
+	            	else 
+	            		continue;	            	
+	            }
+	            if (flag == false)
+	            {
+	            	System.out.println("Я не знаю больше городов на букву " + lastLetter + ". Вы выиграли!");
+	            }
+	            lines.close();
+	            System.out.println("Твой ход на букву " + lastLetter + "!");
+	                   
+                while(true) {
+                	Scanner input = new Scanner(System.in);	  
+                	String userTown = "";
+                	if (input.hasNextLine())
+                	{
+                		String noUseLine = input.nextLine();
+                		userTown = noUseLine;
+                		if (Skeleton.help(noUseLine))
+                			continue;               	                		
+                	}              	
+    	            firstLetter = userTown.substring(0, 1);    	           
+    	            if (!(lastLetter.equals(firstLetter) || lastLetter.toLowerCase().equals(firstLetter)))
+                    {
+                     	System.out.println("Ты играешь не по правилам. Назови город на букву " + lastLetter.toUpperCase());
+                     	continue;
+                    }
+
+    	            String isItACity = сheckCity(userTown, firstLetter);
+    	            if (isItACity.equals("Error"))
+    	            {
+    	            	System.exit(0);
+    	            }
+    	            if (!isItACity.equals("+"))
+    	            {
+    	                System.out.println("Не жульничай! Ты называешь не город:)))");
+	                    continue;
+    	            }
+    	                
+    	            if (checkTheWordInTheDictionary(userTown))
+    	            {
+    	            	lastLetter = check(lastLetter, userTown);
+                 		break;
+    	            }        		
+                 	else 
+                 	{
+                 		System.out.println("Ай-яй-яй! Это слово уже было названо. Попробуй еще раз!");                     
+                 		continue;
+                 	} 
+     	            //input.close(); 
+                }                
+                System.out.println("Мой ход!");             
+	        } 
+			catch (FileNotFoundException e) 
+			{
+	            e.printStackTrace();
+	        } 
+			catch (IOException e) 
+			{
+	            e.printStackTrace();
+	        }
+		}		
+	}
 	
 	public static String check(String lastLetter, String line)
 	{
+        //Если город оканчивается на ЬЪЫЙЁ, то нужно вводить город на предпоследнюю букву
+		//Если город оканчивается на ЫЙ, то нужно вводить город на препредпоследнюю букву
 		if (line.substring(line.length() - 2, line.length()).toUpperCase().equals("ЫЙ"))
 			lastLetter = line.substring(line.length() - 3, line.length() - 2).toUpperCase();
 		else if (lastLetter.equals("Ь") || lastLetter.equals("Ъ") || lastLetter.equals("Ы") || lastLetter.equals("Й") || lastLetter.equals("Ё"))
@@ -25,80 +120,41 @@ public class Towns {
 		return lastLetter;
 	}
 	
-	public static void cities()
+	public static String сheckCity(String userCity, String firstLetter)
 	{
-		String alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЮЯ";
-		Random ran = new Random();
-		int linesCount = 0;
-		int n = ran.nextInt(28);
-		String lastLetter = alphabet.substring(n, n+1);
-		String line = "";
-		while(true)
+		//Этот метод проверяет то, что вводимая пользователем строка является городом.
+		try
 		{
-			try {
-	            File file = new File(System.getProperty("user.dir") + "\\Города\\" + lastLetter + ".txt");
-	            FileReader fr = new FileReader(file);
-	            BufferedReader reader = new BufferedReader(fr);
-	            line = reader.readLine();
-				Path path = Paths.get(System.getProperty("user.dir") + "\\Города\\" + lastLetter + ".txt");
-				Stream<String> lines = Files.lines(path, Charset.defaultCharset());
-				linesCount = (int)lines.count();				
-				n = ran.nextInt(linesCount);
-				line = Files.lines(path, Charset.defaultCharset()).skip(n).findFirst().get();
-	            String firstLetter = "";
-	            Boolean flag = false;
-	            while (flag != true ) {
-	            	                  
-                    //System.out.println("Hi");
-	            	if (usedCities.indexOf(line) == -1)
-	            	{
-	            		System.out.println(line);
-                        usedCities.add(line);
-                        lastLetter = line.substring(line.length() - 1, line.length()).toUpperCase();
-                        lastLetter = check(lastLetter, line);
-        				flag = true;
-	            		break;
-	            	}            		
-	            	else 
-	            		continue;	            	
-	            }
-	            if (line == null)
+			String lineInFile = "";
+			String isItACity = "";
+			LineNumberReader lnr = new LineNumberReader(new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\chatBot\\Города\\" + firstLetter.toUpperCase() + ".txt")));
+	        while((lineInFile = lnr.readLine()) != null)
+	        {
+	            if (lineInFile.equals(userCity) || lineInFile.equals(Character.toString(userCity.charAt(0)).toUpperCase() + userCity.substring(1, userCity.length())))
 	            {
-	            	System.out.println("Я не знаю больше городов на букву " + lastLetter + ". Вы выиграли!");
-	            }
-	            System.out.println("Ваш ход на букву " + lastLetter + "!");
-	            Scanner input = new Scanner(System.in);
-                while(true) {
-                	String userTown = input.nextLine(); 
-                	Boolean chekHelp = Skeleton.help(userTown);
-                	if (chekHelp)
-                		System.out.println("Ничего страшного, продолжай");
-    	            firstLetter = userTown.substring(0, 1);
-                	 if (!(lastLetter.equals(firstLetter)|| lastLetter.toLowerCase().equals(firstLetter) ))
-                     {
-                     	System.out.println("Ты играешь не по правилам. Назови город на букву " + lastLetter.toUpperCase());
-                     	continue;
-                     }
-     	            if (usedCities.indexOf(userTown) == -1 && usedCities.indexOf(userTown.toLowerCase()) == -1 && usedCities.indexOf(Character.toString(userTown.charAt(0)).toUpperCase() + userTown.substring(1, userTown.length())) == -1)
-                 	{  	            	
-                        usedCities.add(Character.toString(userTown.charAt(0)).toUpperCase() + userTown.substring(1, userTown.length()));                       
-                        lastLetter = userTown.substring(userTown.length() - 1, userTown.length()).toUpperCase();
-                        lastLetter = check(lastLetter, userTown);
-                 		break;
-                 	}            		
-                 	else 
-                 	{
-                 		System.out.println("Ай-яй-яй. Это слово уже было названо. Попробуй еще раз!");                     
-                 		continue;
-                 	}    	      
-                }
-                System.out.println("Мой ход!");
-                  
-	        } catch (FileNotFoundException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
+	            	isItACity += "+";
+	            } 
 	        }
+	        lnr.close();
+	        return isItACity;
 		}		
+		catch (IOException e) 
+        {
+           e.printStackTrace();
+           return "Error";
+        } 
+	}
+	
+	public static Boolean checkTheWordInTheDictionary(String userCity)
+	{
+		//Проверка на то, что слово еще не называлось в игре.
+		if (usedCities.indexOf(userCity) == -1 && usedCities.indexOf(userCity.toLowerCase()) == -1 
+				&& usedCities.indexOf(Character.toString(userCity.charAt(0)).toUpperCase() + userCity.substring(1, userCity.length())) == -1)
+     	{  	            	
+            usedCities.add(Character.toString(userCity.charAt(0)).toUpperCase() + userCity.substring(1, userCity.length()));                       
+            lastLetter = userCity.substring(userCity.length() - 1, userCity.length()).toUpperCase();
+            return true;
+     	}
+        return false;
 	}
 }
