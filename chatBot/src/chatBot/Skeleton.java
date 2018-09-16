c18afa64bbbe259b3c2eda18b8b9c7e0bc196587
ekmust2499 <katiammm0204@mail.ru>
2018-09-16 23:48:57 +0500

    Update

--- chatBot/src/chatBot/Skeleton.java
+++ chatBot/src/chatBot/Skeleton.java
@@ -1,14 +1,59 @@
 package chatBot;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Skeleton 
 {
+	private static String[] s_questions = {
+			"Как тебя зовут?",
+			"Как погода за окном?",
+			"Как у тебя настроение?",
+			"Что ты любишь?",
+			"Сколько тебе лет?",
+			"Как часто ты грустишь?",
+			"Что ты ненавидишь делать?",
+			"Меня давно интересовал вопрос, что такое ОПП?",
+			"Сколько времени на часах?",
+			"Расскажи о твоём хобби",
+			"Почему не спишь?",
+			"Каких животных ты любишь?",
+			"Как любишь проводить выходные?",
+			"Что делал на каникулах?",
+			"В чём ты спишь?",
+			"Ты оптимист, пессимист или может быть реалист?",
+			"Какие у тебя увлечения?",
+			"Какой самый счастливый момент в твоей жизни?",
+			"Какую музыку слушаешь последнее время?",
+			"Скажи, а какое твоё любимое время года?",
+			"Какая твоя любимая книга?"
+	};
+	
+	private static String[] s_phrases = {
+			"Оо, как интересно!",
+			"Замечательно!",
+			"Удивительно!", 
+			"Просто чудесно!", 
+			"Великолепно!",
+			"Весьма впечатляет!",
+			"Ты меня удивил!", 
+			"Я ошарашен!", 
+			"Весьма не дурно",
+			"Это серьёзно...",
+			"Идеально!",
+			"Круто!",
+			"Любопытно однако:)",
+			"Потрясающе!", 
+			"Смешно, ха-ха:)",
+			"Хорошо!",
+			"Ясно.",
+			"Чертовски остроумно!"
+	};
+	
     private static String s_aboutMe = "Я бот Евлампий, призванный играть с тобой:))\n"
 			+ "Пиши мне \"help\" в любое время и я напомню как со мной общаться.\n"
 			+ "Я буду предлагать тебе на выбор разные игры,\n"
-			+ "а твоя задача выбрать игру и наслаждаться:))\n"
-			+ "Хочешь ли ты поиграть со мной?";
+			+ "а твоя задача выбрать игру и наслаждаться:))\n";
     
 	public static void main(String[] args) 
 	{
@@ -19,27 +64,34 @@ public class Skeleton
 	
 	public static void start()
 	{
+		Scanner input = new Scanner(System.in);	
+		String s_useResponse;
+		int count;
+		Random randomer1 = new Random();
 		while (true) 
-		{			
-			Scanner input = new Scanner(System.in);
-			String s_useResponse = input.nextLine();
+		{		
+			System.out.println("Поиграем?");
+			s_useResponse = input.nextLine();
 			help(s_useResponse);
 			if (s_useResponse.equals("yes") || s_useResponse.equals("Да") || s_useResponse.equals("да") || s_useResponse.equals("конечно"))  
-			{
+			{				
 				System.out.println("Во что будем играть?");
 				System.out.println("У меня есть интересная игра в \"Города\"");
 			    Towns.gameOfCities();
 			} 
 			else if (s_useResponse.equals("no") || s_useResponse.equals("Нет") || s_useResponse.equals("нет")) 
 			{
-				System.out.println("Извините, я умею только играть:((");
+				count = randomer1.nextInt(s_questions.length);				
+				System.out.println(s_questions[count]);
+				s_useResponse = input.nextLine();
+				count = randomer1.nextInt(s_phrases.length);				
+				System.out.print(s_phrases[count] + " ");
 			} 
 			else
 			{
 				System.out.println("Извините, я вас не понял:((");
-			}		
-			input.close();
-		}
+			}				
+		}		
 	}
 	
 	public static boolean help(String s_Chek) 
@@ -47,7 +99,7 @@ public class Skeleton
 		if (s_Chek.equals("") || s_Chek.equals("help") || s_Chek.equals("помоги")
 				|| s_Chek.equals("помощь") || s_Chek.equals("расскажи о себе")) 
 		{
-			System.out.println(s_aboutMe);
+			System.out.print(s_aboutMe);
 			return true;
 		}
 		return false;
