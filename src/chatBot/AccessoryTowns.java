465210d9ee3de27fa494f8cfcc9226d7b3cf2b72
ekmust2499 <43163063+ekmust2499@users.noreply.github.com>
2018-10-01 22:08:21 +0500

    Update AccessoryTowns.java

--- src/chatBot/AccessoryTowns.java
+++ src/chatBot/AccessoryTowns.java
@@ -1,40 +1,41 @@
-﻿package chatBot;
+package chatBot;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 
 
 public class AccessoryTowns {
 	
-	public String check(String lastLetter, String line)
+	public String getLastSignificantLetter(String line)
 	{
 		//Если город оканчивается на ьйыъё, то нужно вводить город на предпоследнюю букву
 		//Если город оканчивается на ый, то нужно вводить город на препредпоследнюю букву
-		
-		
+		String lastLetter = "";
 		if (line.substring(line.length() - 2, line.length()).endsWith("ый") && line.length() > 2) 
 		{
 			lastLetter = line.substring(line.length() - 3, line.length() - 2);
 		}	
-		else if ((lastLetter.endsWith("ь")
-				|| lastLetter.endsWith("й")
-				|| lastLetter.endsWith("ы")
-				|| lastLetter.endsWith("ъ")
-				|| lastLetter.endsWith("ё")) 
+		else if (( line.endsWith("ь")
+				|| line.endsWith("й")
+				|| line.endsWith("ы")
+				|| line.endsWith("ъ")
+				|| line.endsWith("ё")) 
 				&& line.length() > 1)
 		{
         	lastLetter = line.substring(line.length() - 2, line.length() - 1);
 		}
-
-		return lastLetter.toLowerCase();
+		else
+		{
+			lastLetter = line.substring(line.length() - 1, line.length());
+		}
+		return lastLetter.toLowerCase();	
 	}
-	
+		
 	public boolean checkCity(FileTownsReader reader, String userCity, String firstLetter)
 	{
 		//Этот метод проверяет то, что вводимая пользователем строка является городом.
 		userCity = userCity.toLowerCase();
 
-		ArrayList<String> allTownsOnLetter = reader.dictTowns.get(firstLetter);
+		HashSet <String> allTownsOnLetter = new HashSet<String>(reader.dictTowns.get(firstLetter));
 		for (String town : allTownsOnLetter)
 		{
 			if (town.toLowerCase().equals(userCity))
