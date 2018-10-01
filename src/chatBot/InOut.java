a0021054c3c895ae48be1f0757e5390e1a2af589
ekmust2499 <43163063+ekmust2499@users.noreply.github.com>
2018-10-01 22:33:47 +0500

    Update InOut.java

--- src/chatBot/InOut.java
+++ src/chatBot/InOut.java
@@ -4,29 +4,19 @@ import java.util.Scanner;
 
 public class InOut implements DataReader, DataWriter
 {
+	Scanner input = new Scanner(System.in);
 	public String scanData() 
-	{
-		Scanner input = new Scanner(System.in);
+	{		
 		String s_useResponse  = "";
 		while (checkData(s_useResponse)) 
 		{
 			s_useResponse = input.nextLine();
 		}
 		return s_useResponse;
-	}
-	
-	public boolean checkData(String userData) 
-	{
-		if (userData.equals("")) 
-		{
-			return true;
-		}
-		return false;
-	}
+	}	
 	
 	public void printData(String s_user) 
 	{
 		System.out.println(s_user);
 	}
-
 }
