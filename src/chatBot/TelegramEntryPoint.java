91fe62857d4ab66ea6bd5465007b8da68c1c5f4e
ekmust2499 <katiammm0204@mail.ru>
2018-10-25 21:04:42 +0500

    Update

--- src/chatBot/TelegramEntryPoint.java
+++ src/chatBot/TelegramEntryPoint.java
@@ -3,6 +3,7 @@ package chatBot;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
 
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
